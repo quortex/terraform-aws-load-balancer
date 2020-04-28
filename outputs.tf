74ccfc8ae0c5a942951f9d9564a97765cb8785bc
Pauline Castets <pauline.castets@gmail.com>
2020-04-28 09:25:09 +0200

    Add load balancer

---
+++ outputs.tf
@@ -0,0 +1,35 @@
+/**
+ * Copyright 2020 Quortex
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+output "public_lb_target_group_arns" {
+  value       = aws_lb_target_group.quortex_public[*].arn
+  description = "The ARN of the target groups that instances should be attached to. Once the load balancer and its target groups are created, instances that can listen must be attached to these groups."
+}
+
+output "private_lb_target_group_arns" {
+  value       = aws_lb_target_group.quortex_public[*].arn
+  description = "The ARN of the target groups that instances should be attached to. Once the load balancer and its target groups are created, instances that can listen must be attached to these groups."
+}
+
+output "dns_records_public" {
+  value       = local.public_domains
+  description = "A map with dns records in given dns zone for each dns_records_public provided in variables."
+}
+
+output "dns_records_private" {
+  value       = local.private_domains
+  description = "A map with dns records in given dns zone for each dns_records_private provided in variables."
+}
---
+++ variables.tf
@@ -0,0 +1,94 @@
+/**
+ * Copyright 2020 Quortex
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+variable "name" {
+  type        = string
+  description = "This value will be in the Name tag of all resources."
+}
+
+variable "region" {
+  type        = string
+  description = "The region in wich to create load balancers."
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "ID of the VPC in which the ALB should be."
+}
+
+variable "subnet_ids" {
+  type        = list(string)
+  description = "The IDs of the subnets"
+}
+
+variable "subnet_cidr_blocks" {
+  type        = list(string)
+  description = "CIDR blocks of the subnets"
+}
+
+variable "cluster_security_group_id" {
+  type        = string
+  description = "ID of the cluster security group created by EKS. Rules will be added to it, to allow traffic to the backend nodeports (restricted to the subnet cidr)."
+}
+
+variable "load_balancer_public_app_backend_ports" {
+  type        = list(number)
+  description = "The port number on which backend instances are listening"
+}
+
+variable "load_balancer_private_app_backend_ports" {
+  type        = list(number)
+  description = "The port number on which backend instances are listening"
+}
+
+variable "load_balancer_certificate_arn" {
+  type        = string
+  description = "The ARN identifier of the certificate, from the AWS Certificate Manager service. The dns name defined in the certificate must match the DNS records created in Route53 (variables dns_records_private, dns_records_public)."
+}
+
+variable "load_balancer_private_whitelisted_ips" {
+  type        = list(string)
+  description = "A list of IP ranges to whitelist for private access."
+  default     = []
+}
+
+variable "load_balancer_autoscaling_group_names" {
+  type        = list(string)
+  description = "The name of the autoscaling groups containing the target instances, so that instances can be attached to the load balancer's target group."
+}
+
+variable "dns_hosted_zone_id" {
+  type        = string
+  description = "The ID of the hosted zone in Route53, under which the DNS record should be created."
+}
+
+variable "dns_records_private" {
+  type        = map(string)
+  description = "A map with dns records to add in dns_managed_zone for private endpoints set as value. Full domain names will be exported in a map for the given key."
+  default     = {}
+}
+
+variable "dns_records_public" {
+  type        = map(string)
+  description = "A map with dns records to add in dns_managed_zone for public endpoints set as value. Full domain names will be exported in a map for the given key."
+  default     = {}
+}
+
+variable "resource_labels" {
+  type        = map
+  description = "The labels (a map of key/value pairs) to be applied to created resources."
+  default     = {}
+}
