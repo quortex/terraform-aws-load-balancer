ab6d9358121f509452be1c5e9ea9670f257256a3
Pauline <p.castets@quortex.com>
2020-08-13 17:05:01 +0200

    Fix: use TLS to retrieve content from the origin

--- cdn.tf
+++ cdn.tf
@@ -37,7 +37,7 @@ resource "aws_cloudfront_distribution" "lb_distribution" {
       http_port                = 80
       https_port               = 443
       origin_keepalive_timeout = 5
-      origin_protocol_policy   = "http-only"
+      origin_protocol_policy   = "https-only"
       origin_read_timeout      = 30
       origin_ssl_protocols = [
         "TLSv1",
