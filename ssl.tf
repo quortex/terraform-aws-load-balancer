f820cad13933485db63beda55bb64cd4f9452f17
Vincent Marguerie <v.marguerie@quortex.io>
2021-03-10 14:31:41 +0100

    convert certificates domain_validation_options to list to fix terraform validate

--- ssl.tf
+++ ssl.tf
@@ -42,10 +42,10 @@ resource "aws_acm_certificate" "cert" {
 resource "aws_route53_record" "cert_validation" {
   count = var.ssl_certificate_arn == null ? 1 : 0
 
-  name    = aws_acm_certificate.cert[0].domain_validation_options.0.resource_record_name
-  type    = aws_acm_certificate.cert[0].domain_validation_options.0.resource_record_type
+  name    = tolist(aws_acm_certificate.cert[0].domain_validation_options).0.resource_record_name
+  type    = tolist(aws_acm_certificate.cert[0].domain_validation_options).0.resource_record_type
   zone_id = data.aws_route53_zone.selected[0].zone_id
-  records = [aws_acm_certificate.cert[0].domain_validation_options.0.resource_record_value]
+  records = [tolist(aws_acm_certificate.cert[0].domain_validation_options).0.resource_record_value]
   ttl     = 60
 }
 
